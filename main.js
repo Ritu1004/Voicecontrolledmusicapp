10867f6abaf6a58bfb0066384affdd7de1c03e48
Ritu1004 <tritu084@gmail.com>
2017-07-23 01:20:19 +0530

    Add files via upload
    new addded

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
+              var currentSongNumber = 1;
+		var willLoop = 0;
+		var willShuffle = 0;
+		var Playingnumber = 0  ;
+         var shuffle=0;
+		
+	
+	
+function changeSong() 
+{
+var music =  songs[Playingnumber].fileName;
+var song = document.querySelector("audio");
+song.src = music;
+toggleSong();
+changeCurrentSongDetails(songs[Playingnumber])
+}
+	
+	
+	function toggleSong() {							
+	var song = document.querySelector('audio');
+		if(song.paused == true) {
+		console.log('Playing');
+		$('.play-icon').removeClass('fa-play').addClass('fa-pause');		
+		song.play();
+		}
+		else {
+		console.log('Pausing');
+		$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+		song.pause();
+		}
+		}     
 
+		
 				
-				function fancyTimeFormat(time)
+//--------------------------details of songss--------------------------------		
+						function changeCurrentSongDetails(songobj) {			
+							$('.current-song-image').attr('src','img/' + songobj.image);
+							$('.current-song-name').text(songobj.name);
+							$('.current-song-album').text(songobj.album);
+						}
+						
+						
+						function UpdateTimer() {
+						var song = document.querySelector('audio');
+						var ct = song.currentTime;
+						var td = song.duration;
+						var percentage = (ct/td)*100;
+						$('.progress-filled').css('width', percentage+ "%");
+						}
+						
+	//------------------add fancy time------------------------					
+		
+					function fancyTimeFormat(time)
 			{   
 				// Hours, minutes and seconds
 				var hrs = ~~(time / 3600);
@@ -18,112 +68,119 @@
 				ret += "" + secs;
 				return ret;
 			}
-						
-			function toggleSong() {
-		var song = document.querySelector('audio');
-		if(song.paused == true) {
-		console.log('Playing');
-		$('.play-icon').removeClass('fa-play').addClass('fa-pause');
-		song.play();
-		}
-		else {
-		console.log('Pausing');
-		$('.play-icon').removeClass('fa-pause').addClass('fa-play');
-		song.pause();
-		}
-		}
+				
+	//--------------------end----------------------------------	
 		
-					function updateCurrentTime() {
+		
+			
+	//----------------------function(current time and duration time)----------
+				function updateCurrentTime() {
 					var song = document.querySelector('audio');
 					var currentTime = Math.floor(song.currentTime);
 					currentTime = fancyTimeFormat(currentTime);
 					var duration = Math.floor(song.duration);
-					duration = fancyTimeFormat(duration)
+					duration = fancyTimeFormat(duration);
 					$('.time-elapsed').text(currentTime);
 					$('.song-duration').text(duration);
+					}
+					
+							
+	//---------------------------add timejump to end of the song by -5 and play next--------------
+				function timeJump() {
+				var song = document.querySelector('audio')
+				song.currentTime = song.duration - 5;
 				}
 				
-									function addSongNameClickEvent(songObj,position) {
-									var id = '#song' + position;
-					var songName=songObj.fileName;			
-					$(id).click(function() {
-					var audio = document.querySelector('audio');
-					var currentSong = audio.src;
-					if(currentSong.search(songName) != -1)
-					{
-					toggleSong();
-					}
-					else {
-					audio.src = songName;
-					toggleSong();
-					changecurrentNameDetails(songObj);
-					}
+				
+				
+	//----------when window is loading.....update the first song detail------------			
+			window.onload = function() {
+						changeCurrentSongDetails(songs[0]);   
+					updateCurrentTime(); 
+					setInterval(function() {
+					updateCurrentTime();
+					UpdateTimer();
+					},1000);
+					
+					$('#songs').DataTable({
+						paging: false
 					});
+									
 					}
-				    
-				
-								
-				window.onload = function() {
-				
-				setInterval(function() {
-				updateCurrentTime();
-				},1000);
-				}
-				// var songName1 = 'Chantaje';
-				//var songName2 = 'Let me love you+Tum hi ho';
-				//var songName3 = 'Tera Nasha';
-				//var songName4 = 'Starving';
-				 //var songList = ['Chantaje ','Let me love you+Tum hi ho', 'Tera Nasha', 'Starving'];
-				// var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
-				 //var artistList = ['Shakira ft. Maluma','Vidya Vox','The Bilz & Kashif','Hailee Steinfeld'];
-				// var albumList = ['No album','Vidya Vox Mashup cover','Breaking Barriers','Haiz'];
-				//var durationList = ['3:16','3:10','4:08','3:01'];
-				 
-				 
-						var songs = [{
+		
+					function addSongNameClickEvent(songobj,position) {
+							var songName = songobj.fileName;
+							var id = '#song' + position;
+						$(id).click(function() {
+						var audio = document.querySelector('audio');
+						var currentSong = audio.src;
+						if(currentSong.search(songName) != -1)
+						{
+						toggleSong();
+						}
+						else {
+							
+						audio.src = songName;
+						toggleSong();
+						console.log(obj);
+						changeCurrentSongDetails(songobj);
+						}
+						});
+						}
+
+
+//--------------------------------------------my songs detail---------------------------------//
+
+				   var songs = [{
 					'name': 'Chantaje',
 					'artist': 'Shakira ft. Maluma',
 					'album': 'No album',
 					'duration': '3:16',
-				   'fileName': 'song1.mp3'
+				   'fileName': 'song1.mp3',
+				   'image':'song1.jpg'
 				},
 				{
 					'name': 'Let me love you+Tum hi ho',
 					'artist': 'Vidya Vox',
 					'album': 'Vidya Vox Mashup cover',
 					'duration': '3:10',
-					'fileName': 'song2.mp3'
+					'fileName': 'song2.mp3',
+					'image':'song2.jpg'
 				},
 				{
 					'name': 'Tera Nasha',
 					'artist': 'The Bilz & Kashif',
 					'album': 'Breaking Barriers',
 					'duration': '4:08',
-					'fileName': 'song3.mp3'
+					'fileName': 'song3.mp3',
+					'image':'song3.jpg'
 				},
 				{
 					'name': 'Starving',
 					'artist': 'Hailee Steinfeld',
 					'album': 'Haiz',
 					'duration': '3:01',
-					'fileName': 'song4.mp3'
+					'fileName': 'song4.mp3',
+					'image':'song4.jpg'
 				}]
-							
 				
-				
-				
-						 for(var i =0; i < songs.length;i++) {
-								 var obj = songs[i];
-						var name = '#song' + (i+1);
-						var song = $(name);
-						song.find('.song-name').text(obj.name);
-						song.find('.song-artist').text(obj.artist);
-						song.find('.song-album').text(obj.album);
-						song.find('.song-length').text(obj.duration);
-						addSongNameClickEvent(obj,i+1);
+	//-------------------------------------------------loop part----------------------//
+			
+								
+					for(var i =0; i < songs.length;i++) {
+						var obj = songs[i];
+							var name = '#song' + (i+1);
+							var song = $(name);
+							song.find('.song-name').text(obj.name);
+							song.find('.song-artist').text(obj.artist);
+							song.find('.song-album').text(obj.album);
+							song.find('.song-length').text(obj.duration); 
+							 addSongNameClickEvent(obj,i+1)
 						}
-										
+						
 				
+		
+
 						//addSongNameClickEvent(fileNames[0],1);
 						
 						//addSongNameClickEvent(fileNames[1],2);
@@ -136,15 +193,61 @@
 						//addSongNameClickEvent(fileNames[i],i+1)
 						//}
 					    
-								
-				
-								
+						//	$('#songs').DataTable();
+					//		}
+										
+									
+						$('.fa-repeat').on('click',function() {
+							$('.fa-repeat').toggleClass('disabled')
+							willLoop = 1 - willLoop;
+							});	
+												
+							$('.fa-random').on('click',function() {
+							$('.fa-random').toggleClass('disabled');
+							willShuffle = 1 - willShuffle;
+							});	
 							
-	
-    $('.welcome-screen button').on('click', function() {
+							$('audio').on('ended',function() {
+								var audio = document.querySelector('audio');
+								if (willShuffle == 1) {
+									var nextSongNumber = randomExcluded(1,4,currentSongNumber); //calling our function for stackoverflow 
+									var nextSongobj = songs[nextSongNumber-1];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber = nextSongNumber;
+								}
+								else if(currentSongNumber < 4) {
+									var nextSongobj = songs[currentSongNumber];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber = currentSongNumber + 1;
+								}
+								else if(willLoop == 1) {
+									var nextSongobj = songs[0];
+									audio.src = nextSongobj.fileName;
+									toggleSong();
+									changeCurrentSongDetails(nextSongobj);
+									currentSongNumber =  1;
+								}
+								else {
+									$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+									audio.currentTime = 0;
+								}
+							});
+							
+
+
+
+//----------------------------------------------------------last part i.e welcome screen wala part-----------------------//									
+								
+										
+	//------ welcome screen----------------					
+		$('.welcome-screen button').on('click', function() {
         var name = $('#name-input').val();
         if (name.length > 2) {
-            var message = "Welcome, " + name;
+            var message = "Welcome to Music app, " + name;
             $('.main .user-name').text(message);
             $('.welcome-screen').addClass('hidden');
             $('.main').removeClass('hidden');
@@ -152,11 +255,75 @@
             $('#name-input').addClass('error');
         }
     });
+	//---------------song play and pause by press keyboard-------------
     $('.play-icon').on('click', function() {
-        toggleSong()
+        toggleSong();
     });
-    $('body').on('keypress', function(event) {
-                if (event.keyCode == 32) {
-                   toggleSong()
-                }
-            });
+			$('body').on('keypress',function(event) {
+			var target = event.target;
+			if (event.keyCode == 32 && target.tagName !='INPUT')     
+				{
+				toggleSong();
+			}
+		});
+	$('.main button').on('click', function() {
+				$('.main').addClass('hidden');
+            $('.welcome-screen').removeClass('hidden');
+			
+			
+			
+				});
+
+	
+			
+			
+			
+//-----------------------forward function------------------------
+		
+
+					$(".fa-step-forward").click(function(){
+
+					if(shuffle==1)
+					{
+					var audio = document.querySelector('audio');
+					var nextSongNumber = randomExcluded(0,3,Playingnumber); // Calling our function from Stackoverflow
+
+					var nextSongObj = songs[nextSongNumber];
+					audio.src = nextSongobj.fileName;
+					toggleSong();
+					changeCurrentSongDetails(nextSongobj);
+					Playingnumber = nextSongNumber;
+					}
+                   else {
+
+					if(Playingnumber == songs.length-1){
+					Playingnumber = 0;
+					changeSong();
+					}
+
+					else {
+					console.log("two");
+					console.log(Playingnumber);
+					Playingnumber++;
+					changeSong();
+					}}
+					})
+
+                   $(".fa-step-backward").click(function(){
+
+					if(Playingnumber == 0){
+					console.log("one");
+					Playingnumber = (songs.length-1);
+					changeSong();
+					}
+                    else {
+					console.log("two");
+					console.log(Playingnumber);
+					Playingnumber--;
+					changeSong();
+					}
+                      })
+			
+											
+										
+				
